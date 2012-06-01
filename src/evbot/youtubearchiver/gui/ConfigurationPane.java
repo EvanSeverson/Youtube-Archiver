65729f2881b8ebd44c9eba7f1bc8b45a02ed102e
Evan Severson <severson.evbot@gmail.com>
2012-06-01 18:47:14 -0500

    GUI configuration update

--- src/evbot/youtubearchiver/gui/ConfigurationPane.java
+++ src/evbot/youtubearchiver/gui/ConfigurationPane.java
@@ -1,19 +1,50 @@
 package evbot.youtubearchiver.gui;
 
-import java.awt.Dimension;
+import java.awt.Component;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
 
+import javax.swing.ButtonGroup;
 import javax.swing.JButton;
+import javax.swing.JCheckBox;
 import javax.swing.JComponent;
+import javax.swing.JFileChooser;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JRadioButton;
 import javax.swing.JTextField;
 
-public class ConfigurationPane extends JPanel {
+public class ConfigurationPane extends JPanel implements ActionListener, ItemListener{
 	
-	public JTextField saveDirectory;
-	private JButton saveDirectoryBrowse;
+	public static final long serialVersionUID = 1L;
+	
+	public JTextField saveDirectoryTextField;
+	public JButton saveDirectoryBrowseButton;
+	public JFileChooser saveDirectoryFileChooser;
+	public ConfigurationField saveDirectoryConfigurationPane;
+	
+	public JTextField downloadSpeedLimitTextField;
+	public JLabel downloadSpeedLimitLabel;
+	public ConfigurationField downloadSpeedLimitConfigurationField;
+	
+	public JTextField maxThreadsTextField;
+	public ConfigurationField maxThreadsConfigurationField;
+	
+	public JCheckBox zipVideosCheckBox;
+	public ConfigurationField zipVideosConfigurationField;
+	
+	
+	public JCheckBox timeStampCheckBox;
+	public ConfigurationField timeStampConfigurationField;
+	
+	public JButton save;
 	
 	public ConfigurationPane() {
 		
@@ -23,30 +54,109 @@ public class ConfigurationPane extends JPanel {
 	
 	private void init() {
 		
-		setLayout(new GridLayout(1, 0));
-		saveDirectory = new JTextField(40);
-		saveDirectoryBrowse = new JButton("...");
+		setLayout(new GridLayout(0, 1));
 		
-		ConfigurationField saveDirectoryConfigurationPane = 
-				new ConfigurationField("Save to:", saveDirectory, saveDirectoryBrowse);
+		saveDirectoryTextField = new JTextField(40);
+		try {
+			saveDirectoryTextField.setText(
+					this.getClass().getProtectionDomain().getCodeSource()
+					.getLocation().toURI().getPath()
+					+ "Youtube-Archives" +  File.separator);
+		} catch (URISyntaxException e) {
+		}
+		saveDirectoryBrowseButton = new JButton("...");
+		saveDirectoryFileChooser = new JFileChooser();
+		saveDirectoryBrowseButton.setActionCommand("browse");
+		saveDirectoryBrowseButton.addActionListener(this);
+		saveDirectoryFileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+		saveDirectoryFileChooser.addActionListener(this);
+		saveDirectoryConfigurationPane = 
+				new ConfigurationField("Save to:", saveDirectoryTextField,
+						saveDirectoryBrowseButton);
+		
+		downloadSpeedLimitTextField = new JTextField(10);
+		downloadSpeedLimitTextField.setText("0");
+		downloadSpeedLimitLabel = new JLabel("KB/s");
+		downloadSpeedLimitConfigurationField =
+				new ConfigurationField("Download Speed Limit:", downloadSpeedLimitTextField, downloadSpeedLimitLabel);
+		
+		maxThreadsTextField = new JTextField(5);
+		maxThreadsTextField.setText("15");
+		maxThreadsConfigurationField =
+				new ConfigurationField("Max Downlaod Threads:", maxThreadsTextField);
+		
+		zipVideosCheckBox = new JCheckBox();
+		zipVideosCheckBox.setSelected(true);
+		zipVideosConfigurationField =
+				new ConfigurationField("Zip Videos:", zipVideosCheckBox);
+		
+		timeStampCheckBox = new JCheckBox();
+		timeStampCheckBox.setSelected(false);
+		timeStampCheckBox.addItemListener(this);
+		timeStampConfigurationField =
+				new ConfigurationField("Timestamp video:", timeStampCheckBox);
+		
+		save = new JButton("Save");
+		save.addActionListener(this);
 		
 		add(saveDirectoryConfigurationPane);
+		add(downloadSpeedLimitConfigurationField);
+		add(maxThreadsConfigurationField);
+		add(zipVideosConfigurationField);
+		add(timeStampConfigurationField);
+		add(save);
 		
 	}
 	
 	private class ConfigurationField extends JPanel {
 		
-		public ConfigurationField(String label, JComponent... components) {
+		private static final long serialVersionUID = 1L;
+
+		public ConfigurationField(String label, Component... components) {
 			
-			setLayout(new FlowLayout(FlowLayout.TRAILING));
-			
+			setLayout(new FlowLayout(FlowLayout.LEADING));
 			add(new JLabel(label));
-			for(JComponent c : components) {
+			
+			for(Component c : components) {
 				add(c);
 			}
 			
 		}
 		
+		@Override
+		public void setEnabled(boolean enabled) {
+			for(Component c : getComponents()) {
+				c.setEnabled(enabled);
+			}
+			super.setEnabled(enabled);
+		}
 	}
-	
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		
+		String command = e.getActionCommand();
+		System.out.println(command);
+		
+		if(command.equals("browse")) {
+			
+			int returnVal = saveDirectoryFileChooser.showOpenDialog(this);
+			
+			if(returnVal == JFileChooser.APPROVE_OPTION) {
+				try {
+					
+					saveDirectoryTextField.setText(saveDirectoryFileChooser.getSelectedFile().getCanonicalPath());
+					
+				} catch (IOException e1) {
+					e1.printStackTrace();
+				}
+			}
+		}
+		
+	}
+
+	@Override
+	public void itemStateChanged(ItemEvent e) {
+		
+	}
 }
--- src/evbot/youtubearchiver/gui/MainFrame.java
+++ src/evbot/youtubearchiver/gui/MainFrame.java
@@ -1,22 +1,40 @@
 package evbot.youtubearchiver.gui;
 
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
 import javax.swing.UIManager;
-import javax.swing.UnsupportedLookAndFeelException;
+import javax.swing.WindowConstants;
 
-public class MainFrame extends JFrame{
+public class MainFrame extends JFrame implements ActionListener{
+	
+	private static final long serialVersionUID = 1L;
 	
 	private JTabbedPane tabbedPane;
-	public ConfigurationPane configurationPane;
+	private ConfigurationPane configurationPane;
+	
+	private JButton ok;
+	private JButton cancel;
 	
 	public MainFrame() {
 		
+		setTitle("Youtube-Archiver");
 		init();
 		
 	}
 	
 	private void init() {
+		
+//		setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		
+		setLayout(new BorderLayout());
 		
 		try {
 			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
@@ -28,8 +46,28 @@ public class MainFrame extends JFrame{
 		configurationPane = new ConfigurationPane();
 		
 		tabbedPane.addTab("Configuration", configurationPane);
-		add(tabbedPane);
+		
+		ok = new JButton("OK");
+		cancel = new JButton("Cancel");
+		
+		ok.addActionListener(this);
+		cancel.addActionListener(this);
+		
+		JPanel controls = new JPanel(new FlowLayout(FlowLayout.LEFT));
+		controls.add(ok);
+		controls.add(cancel);
+		
+		add(tabbedPane, BorderLayout.CENTER);
+		add(controls, BorderLayout.SOUTH);
+		
 		pack();
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		
+		System.out.println(e.getActionCommand());
 		
 	}
 	
